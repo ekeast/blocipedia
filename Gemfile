00dce076a0f682c4cca9bc56bb4d2497f2da19d2
Ted <ekeast328@gmail.com>
2016-03-19 11:32:56 -0400

    Checkpoint 6 Completed

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'pundit'
 
+gem 'faker'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create!(
+  username: Faker::Internet.user_name,
+  email: Faker::Internet.email,
+  password: Faker::Internet.password(8, 15)
+  )
+end
+
+users = User.all
+
+20.times do
+  Wiki.create!(
+  user: users.sample,
+  title: Faker::Hipster.sentence,
+  body: Faker::Lorem.paragraph,
+  private: Faker::Boolean.boolean
+  )
+end
+
+user = User.first
+user.update_attributes!(
+  username: 'ekeast',
+  email: 'ekeast328@gmail.com',
+  password: 'password'
+)
+
+puts "Seeds finished!"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
